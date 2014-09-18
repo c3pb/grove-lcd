934ac81c8f902e5d96d59bb19f36b23f55e209a3
Gregor Best <gbe@unobtanium.de>
2014-09-18 15:59:21 +0200

    Add dummy terminal for testing
    requires a 256 color capable X terminal, for example Gnome Terminal,
    UXterm, st, etc...

--- display.py
+++ display.py
@@ -1,8 +1,19 @@
-from smbus import SMBus
+#!/usr/local/bin/python2.7
+
 import math
 import time
-import backlight
-import screen
+import sys
+
+use_dummy = False
+try:
+    from smbus import SMBus
+except ImportError:
+    print("Couldn't import smbus bindings, using dummy display")
+    use_dummy = True
+
+if not use_dummy:
+    import backlight
+    import screen
 
 class Display(object):
     backlight = None
@@ -21,13 +32,54 @@ class Display(object):
     def move(self, col, row):
         self.screen.setCursor(col, row)
 
-if __name__ == "__main__":
-    cnt = 0
+class DummyDisplay(object):
+    def __init__(self, bus=None):
+        self.pos  = (0, 0)
+        self.rows = [ " " * 16, " " * 16 ]
+        sys.stdout.write(chr(0x1b) + "[2J")
+        sys.stdout.flush()
 
-    d = Display(SMBus(1))
+    def write(self, text):
+        t  = self.rows[self.pos[1]][:self.pos[0]]
+        t += text
+        t += self.rows[self.pos[1]][-(16 - len(text) - self.pos[0]):]
+        self.rows[self.pos[1]] = t[0:16]
+        self.redraw()
+
+    def color(self, r, g, b):
+        f = 43.0
+        assert r >= 0 and r < 256
+        assert g >= 0 and g < 256
+        assert b >= 0 and b < 256
+        n = (int(r / f) * 36) + (int(g / f) * 6) + int(b / f) + 16
+        sys.stdout.write(chr(0x1b) + "[48;5;" + str(n) + "m")
+        self.redraw()
+
+    def move(self, col, row):
+        assert row == 0 or row == 1
+        assert col >= 0 and col < 16
+        self.pos = (col, row)
+
+    def updatecursor(self, col, row):
+        sys.stdout.write(chr(0x1b) + "[" + str(row) + ";" + str(col) + "H")
+
+    def redraw(self):
+        self.updatecursor(0, 0)
+        sys.stdout.write("{}\n{}".format(self.rows[0], self.rows[1]))
+        sys.stdout.flush()
+        pass
+
+if __name__ == "__main__":
+    d = None
+    if use_dummy:
+        d = DummyDisplay()
+        pass
+    else:
+        d = Display(SMBus(1))
     d.move(0, 0)
     d.write("Yeah.      Nice.")
 
+    cnt = 0
     while True:
         r = int((math.sin(cnt) + 1) * 128)
         g = int((math.sin(cnt + 0.75 * math.pi) + 1) * 128)
@@ -38,5 +90,8 @@ if __name__ == "__main__":
         # d.move(0, 1)
         # d.write("{:>3} {:>3} {:>3}".format(r, g, b))
 
-        cnt += 0.01
-        time.sleep(0.00001)
+        if use_dummy:
+            cnt += 0.25
+        else:
+            cnt += 0.01
+        time.sleep(0.01)
